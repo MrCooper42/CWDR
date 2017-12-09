5405afe1ce1cdc9f5e7d1e11899c88b9269d7163
MrCooper42 <mr.cooper42co@gmail.com>
2017-12-08 21:22:36 -0700

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
