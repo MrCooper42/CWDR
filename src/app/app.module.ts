4b9ce7dc4d928f81ca9018c45b2731765ed5bd71
MrCooper42 <mr.cooper42co@gmail.com>
2017-12-10 16:07:19 -0700

    footer componenet added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
